5bfd6d5cdc72d8743497b9af3d679b63212c881d
Ritesh Birthal <riteshb@iitk.ac.in>
2021-08-14 09:19:22 +0530

    databaseurl

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Server
   PORT: process.env.PORT || 3000,
   // Database
-  DBURL: 'mongodb://localhost:27017/ritesh',
+  DBURL: 'mongodb+srv://ritesh:birthal@cluster0.kjmac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   // Secrets
   AUTHSECRET: 'd428546e65be4c9b804b21fee2c8262453a4e2c0ba76f6ba6454950df4fe1b7b7964312187ec24ac11d05f7495aff503bd00e918870b6dd6525409eb0a83f617',
   PWDSALT: '6f9f1f7fdbbd61af35ebc168b2a25a83',
